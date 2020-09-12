53f50d3c5cdd0ea59b3a449ae0b40dea3586cf45
syki66 <66syki@gmail.com>
2020-09-13 04:03:30 +0900

    add camera streaming code

--- picar_server/app.py
+++ picar_server/app.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template
 import RPi.GPIO as gpio
 import time
+from flask import Flask, render_template, Response
+from camera_pi import Camera
 
 def init():
     gpio.setmode(gpio.BOARD)
@@ -65,18 +66,32 @@ directions = [forward, reverse, turn_left, turn_right, pivot_left, pivot_right]
 app = Flask(__name__)
 
 
-
-
 @app.route('/dc_motor/<int:id>')
 def move(id):
 	init()
 	directions[id](t)
 
-	return render_template('test.html')
+	return render_template('dc_motor.html')
+
+
 
 @app.route('/')
-def hello():
-	return "helloworld"
+def index():
+    """Video streaming home page."""
+    return render_template('index.html')
 
-if __name__ == "__main__":
-	app.run(host="0.0.0.0", port="80", debug=True)
+def gen(camera):
+    """Video streaming generator function."""
+    while True:
+        frame = camera.get_frame()
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
+@app.route('/video_feed')
+def video_feed():
+    """Video streaming route. Put this in the src attribute of an img tag."""
+    return Response(gen(Camera()),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port =80, debug=True, threaded=True)
