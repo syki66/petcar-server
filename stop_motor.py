bebb15f4d11b18e211a26148a0d193a146d2585d
syki66 <66syki@gmail.com>
2020-09-12 20:34:46 +0900

    add stop_motor.py

---
+++ stop_motor.py
@@ -0,0 +1,13 @@
+import RPi.GPIO as gpio
+
+
+def init():
+    gpio.setmode(gpio.BOARD)
+    gpio.setup(7, gpio.OUT)
+    gpio.setup(11, gpio.OUT)
+    gpio.setup(13, gpio.OUT)
+    gpio.setup(15, gpio.OUT)
+
+init()
+
+gpio.cleanup()
