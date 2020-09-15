4e58af6d7c148dfee110e29474b96d2fc160e022
syki66 <66syki@gmail.com>
2020-09-16 00:45:21 +0900

    use httpget method

--- picar_server/dc_motor.py
+++ picar_server/dc_motor.py
@@ -1,63 +1,65 @@
 import RPi.GPIO as gpio
 import time
 
+pins = (7, 11, 13, 15)
+
 def init():
     gpio.setmode(gpio.BOARD)
-    gpio.setup(7, gpio.OUT)
-    gpio.setup(11, gpio.OUT)
-    gpio.setup(13, gpio.OUT)
-    gpio.setup(15, gpio.OUT)
+    gpio.setup(pins[0], gpio.OUT)
+    gpio.setup(pins[1], gpio.OUT)
+    gpio.setup(pins[2], gpio.OUT)
+    gpio.setup(pins[3], gpio.OUT)
 
 def forward(tf):
     init()
-    gpio.output(7, True)
-    gpio.output(11, False)
-    gpio.output(15, True)
-    gpio.output(13, False)
+    gpio.output(pins[0], True)
+    gpio.output(pins[1], False)
+    gpio.output(pins[3], True)
+    gpio.output(pins[2], False)
     time.sleep(tf)
     gpio.cleanup()
 
 def reverse(tf):
     init()
-    gpio.output(7, False)
-    gpio.output(11, True)
-    gpio.output(15, False)
-    gpio.output(13, True)
+    gpio.output(pins[0], False)
+    gpio.output(pins[1], True)
+    gpio.output(pins[3], False)
+    gpio.output(pins[2], True)
     time.sleep(tf)
     gpio.cleanup()
 
 def turn_left(tf):
     init()
-    gpio.output(7, True)
-    gpio.output(11, False)
-    gpio.output(15, True)
-    gpio.output(13, True)
+    gpio.output(pins[0], True)
+    gpio.output(pins[1], False)
+    gpio.output(pins[3], True)
+    gpio.output(pins[2], True)
     time.sleep(tf)
     gpio.cleanup()
 
 def turn_right(tf):
     init()
-    gpio.output(7, True)
-    gpio.output(11, True)
-    gpio.output(15, True)
-    gpio.output(13, False)
+    gpio.output(pins[0], True)
+    gpio.output(pins[1], True)
+    gpio.output(pins[3], True)
+    gpio.output(pins[2], False)
     time.sleep(tf)
     gpio.cleanup()
 
 def pivot_right(tf):
     init()
-    gpio.output(7, False)
-    gpio.output(11, True)
-    gpio.output(15, True)
-    gpio.output(13, False)
+    gpio.output(pins[0], False)
+    gpio.output(pins[1], True)
+    gpio.output(pins[3], True)
+    gpio.output(pins[2], False)
     time.sleep(tf)
     gpio.cleanup()
 
 def pivot_left(tf):
     init()
-    gpio.output(7, True)
-    gpio.output(11, False)
-    gpio.output(15, False)
-    gpio.output(13, True)
+    gpio.output(pins[0], True)
+    gpio.output(pins[1], False)
+    gpio.output(pins[3], False)
+    gpio.output(pins[2], True)
     time.sleep(tf)
     gpio.cleanup()
