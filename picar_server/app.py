8d3d97164909a2a9245f89096a8f60c590bc068a
syki66 <66syki@gmail.com>
2020-09-15 18:37:14 +0900

    modulize python file

--- picar_server/app.py
+++ picar_server/app.py
@@ -2,83 +2,30 @@ import RPi.GPIO as gpio
 import time
 from flask import Flask, render_template, Response
 from camera_pi import Camera
+from dc_motor import *
 
-def init():
-    gpio.setmode(gpio.BOARD)
-    gpio.setup(7, gpio.OUT)
-    gpio.setup(11, gpio.OUT)
-    gpio.setup(13, gpio.OUT)
-    gpio.setup(15, gpio.OUT)
 
-def forward(tf):
-    gpio.output(7, True)
-    gpio.output(11, False)
-    gpio.output(15, True)
-    gpio.output(13, False)
-    time.sleep(tf)
-    gpio.cleanup()
 
-def reverse(tf):
-    gpio.output(7, False)
-    gpio.output(11, True)
-    gpio.output(15, False)
-    gpio.output(13, True)
-    time.sleep(tf)
-    gpio.cleanup()
-
-def turn_left(tf):
-    gpio.output(7, True)
-    gpio.output(11, False)
-    gpio.output(15, True)
-    gpio.output(13, True)
-    time.sleep(tf)
-    gpio.cleanup()
-
-def turn_right(tf):
-    gpio.output(7, True)
-    gpio.output(11, True)
-    gpio.output(15, True)
-    gpio.output(13, False)
-    time.sleep(tf)
-    gpio.cleanup()
-
-def pivot_right(tf):
-    gpio.output(7, False)
-    gpio.output(11, True)
-    gpio.output(15, True)
-    gpio.output(13, False)
-    time.sleep(tf)
-    gpio.cleanup()
-
-def pivot_left(tf):
-    gpio.output(7, True)
-    gpio.output(11, False)
-    gpio.output(15, False)
-    gpio.output(13, True)
-    time.sleep(tf)
-    gpio.cleanup()
-
-t = 0.1
-
-directions = [forward, reverse, turn_left, turn_right, pivot_left, pivot_right]
 
 
 app = Flask(__name__)
 
+@app.route('/')
+def index():
+    return render_template('index.html')
 
 @app.route('/dc_motor/<int:id>')
 def move(id):
-	init()
-	directions[id](t)
+    """DC motor control page"""
+    t = 0.1
+    directions = [forward, reverse, turn_left, turn_right, pivot_left, pivot_right]
+    directions[id](t)
+    return render_template('dc_motor.html')
 
-	return render_template('dc_motor.html')
-
-
-
-@app.route('/')
-def index():
+@app.route('/camera')
+def camera():
     """Video streaming home page."""
-    return render_template('index.html')
+    return render_template('camera.html')
 
 def gen(camera):
     """Video streaming generator function."""
